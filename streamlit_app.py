262a1f3494eb2105ba08f9f4858f4fb741abd2e3
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2023-06-18 22:19:25 +0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,28 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 import streamlit as st
-conn =  pyodbc.connect(
-    Trusted_Connection='Yes',
-    Driver='{ODBC Driver 17 for SQL Server}',
-    Server='EVOL',
-    Database='BKLIGHT'
-)
+
+#conn =  pyodbc.connect(
+#    Trusted_Connection='Yes',
+#    Driver='{ODBC Driver 17 for SQL Server}',
+#    Server='EVOL',
+#    Database='BKLIGHT'
+#)
+
+
+@st.cache_resource
+def init_connection():
+    return pyodbc.connect(
+        Trusted_Connection=st.secrets["Trusted"],
+        Driver='{ODBC Driver 17 for SQL Server}',
+    Server=st.secrets["Server"],
+    Database=st.secrets["Database"]
+    )
+
+conn = init_connection()
+
+
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM account")
 rows = cursor.fetchall()
-st.write(rows[0][0])
+st.write(rows[0])
