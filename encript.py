9a379c06f24d2b7f299ad9ac057dbfc68d48274f
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2023-08-29 22:36:48 +0700

    Update encript.py

--- encript.py
+++ encript.py
@@ -1,28 +1,20 @@
 
-from cryptography.fernet import Fernet
+#from cryptography.fernet import Fernet
 import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
-from  PIL import Image
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import folium
-import supabase as sb
-from supabase import create_client, Client
-import streamlit_app as sa
-message = "hello geeks"
-key = Fernet.generate_key()
 
-fernet = Fernet(key)
+import pandas as pd
+import streamlit_app as sa
+#key = Fernet.generate_key()
+
+#fernet = Fernet(key)
 
 def encodef(stringt):  
-    return fernet.encrypt(stringt.encode())
+    return #fernet.encrypt(stringt.encode())
 def decodef(stringt):
-    return fernet.decrypt(stringt).decode()
+    return #fernet.decrypt(stringt).decode()
 @st.cache_resource
 def login(username, password):
-    username = decodef(encodef(username))
+    #username = decodef(encodef(username))
     if pd.DataFrame(sa.supabase.table('Account').select("*").eq('user',username).eq('password',password).execute().data).empty:
         name = ""
         return False, name
