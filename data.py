1f65cd3427d1238fd9f249e1a247fff1e937db7d
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2023-09-13 00:39:20 +0700

    s

--- data.py
+++ data.py
@@ -9,32 +9,39 @@ key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6
 supabase = create_client(url, key)
 
 def get_noti():
-    data1 = pd.DataFrame(supabase.table("Events").select("*").execute().data)
-    data1['timestamp'] = pd.to_datetime(data1["timestamp"])
-    data1.sort_values(by='timestamp',ascending=False)   
-    st.write(data1.head(5))
 
+    data1 = pd.DataFrame(supabase.table("Events").select("*").execute().data)
+    if  not data1.empty:
+        data1['timestamp'] = pd.to_datetime(data1["timestamp"])
+        df = data1.sort_values(by='timestamp',ascending=False)
+        sorted_df = df.sort_values(by=['lampid', 'timestamp'], ascending=[True, False])
+
+    # Select the first row within each 'lampid' group (the one with the latest timestamp)
+        latest_rows = sorted_df.groupby('lampid').head(1)
+        
+
+        st.write(latest_rows)
+
+    
     node1 = pd.DataFrame(supabase.table("NodeDeath").select("*").execute().data)
-    node1['time'] = pd.to_datetime(node1["time"])
-    node1.sort_values(by='time',ascending=False)   
-    node1 = node1.head(10)
-    for i in node1.iterrows():
-     
-        if int(i[1].status) == 1:
-            st.warning("Lamp {} is dead at {}".format(i[1].address,i[1].time))
+    if not node1.empty:
+        node1['time'] = pd.to_datetime(node1["time"])
+        node1 = node1.sort_values(by='time',ascending=False)   
+        node1 = node1.head(10)
+        
+        for i in node1.iterrows():    
+            if int(i[1].status) == 1:
+                st.warning("Lamp {} is dead at {}".format(i[1].address,i[1].time))
 
 
     gate = pd.DataFrame(supabase.table("GateAlive").select("*").execute().data)
-    gate['time'] = pd.to_datetime(gate["time"])
-    gate.sort_values(by='time',ascending=False)   
-    gate = gate.head(10)
-    date_format = "%Y-%m-%d %H:%M:%S"
-    
-
-    for i in gate.iterrows():
-        if int(i[1].status) == 1:
-
-            st.error("Node was dead at {}".format(i[1].time))
+    if (not gate.empty):
+        gate['time'] = pd.to_datetime(gate["time"])
+        gate = gate.sort_values(by='time',ascending=False)   
+        gate = gate.head(10)
+        for i in gate.iterrows():
+            if int(i[1].status) == 1:
+                st.error("Gateway was dead at {}".format(i[1].time))
         
 
     
