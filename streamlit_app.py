362fee4857e6bb3aa2f4682129514906e23caf0f
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2023-09-15 00:00:47 +0700

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ elif choose == "Controls":
         keyAll = "sliderAll"    
         def bright_changeAll():  
            for i in data.light["id"]:          
-                bright_client("65535 "+ str(st.session_state["slider"+str(i)]))
+                bright_client(str(i)+" "+ str(st.session_state["slider"+str(i)]))
         def slide_change(value):
             st.session_state[keyAll] = value
         if keyAll not in st.session_state:
